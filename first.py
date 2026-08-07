36b0b2eb48ec4a5f9bcea6b64dfd2a579b598617
Aishwarya <aishwaryazinjurte2003@gmail.com>
2026-08-07 12:47:56 +0530

    Added NumPy practice programs

---
+++ first.py
@@ -0,0 +1,17 @@
+
+var=[1,2,3,4,5,6,7,8,9]
+print(var)     #list
+
+
+#to print array
+
+import numpy as np
+x=np.array([1,2,3,4,5,6,7,8,9])
+print(x)
+
+
+#To check the data type of array
+x=[1,2,3,4,5]
+y=(np.array(x))
+print(y)
+print(type(y))
