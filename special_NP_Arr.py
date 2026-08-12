0e1962a78f3a90260baaa2f74ad475857e4a9d61
Aishwarya <aishwaryazinjurte2003@gmail.com>
2026-08-12 17:24:11 +0530

    creating special numpy array

---
+++ special_NP_Arr.py
@@ -0,0 +1,37 @@
+import numpy as np
+#array zeros
+ar_zero = np.zeros(4)
+print(ar_zero)
+
+# Array Ones
+ar_zeros1= np.zeros((3,4))
+print(ar_zeros1)
+
+ar_one=np.ones(4)
+print(ar_one)
+
+#Create an empty array
+ar_em = np.empty(4)
+print(ar_em)
+
+#Range Element
+ar_rn=np.arange(4)
+print(ar_rn)
+
+#Diagonal element with 1's
+ar_dia=np.eye(3)
+print(ar_dia)
+
+ar_dia=np.eye(3,5)
+print(ar_dia)
+
+#LinSpace
+ar_lin=np.linspace(1,10,num=5)
+print(ar_lin)
+
+
+
+
+
+
+
