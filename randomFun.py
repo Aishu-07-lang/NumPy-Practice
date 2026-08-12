c874c13e3de23e46b1058e16fa43628d0cf1bd89
Aishwarya <aishwaryazinjurte2003@gmail.com>
2026-08-12 17:49:25 +0530

    creating random() values

---
+++ randomFun.py
@@ -0,0 +1,17 @@
+#creating random values 
+var=np.random.rand(4)
+print(var)
+
+var_1=np.random.rand(2,3)
+print(var_1)
+
+
+var1=np.random.ranf(2)
+print(var1)
+
+var2=np.random.randn(5)
+print(var2)
+
+var3=np.random.randint(1,3,4)
+print(var3)
+
--- special_NP_Arr.py
+++ special_NP_Arr.py
@@ -29,7 +29,27 @@ print(ar_dia)
 ar_lin=np.linspace(1,10,num=5)
 print(ar_lin)
 
+ar_one=np.zeros(6)
+print(ar_one)
 
+ar_emp=np.empty(4)
+print(ar_emp)
+
+var=np.random.rand(4)
+print(var)
+
+var_1=np.random.rand(2,3)
+print(var_1)
+
+
+var1=np.random.ranf(2)
+print(var1)
+
+var2=np.random.randn(5)
+print(var2)
+
+var3=np.random.randint(1,3,4)
+print(var3)
 
 
 
